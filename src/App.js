c3e0a2b95635a9bd42be3adf05cf62979de5955e
Anup-Dhakal <anup.dhakal007@gmail.com>
2025-07-04 08:05:09 +0545

    Interest Calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import './index.css'
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
 
-function App() {
+
+const App = () => {
+  const [p,setP] =useState(0);
+  const [t,sett] =useState(0);
+  const [r,setr] =useState(0);
+  const [si,setsi] =useState(0);
+
+  const calculateInterest = () => {
+    setsi((p*t*r)/100);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "div">
+      <AppBar position="static">
+        <Toolbar>
+        Simple Interest
+        </Toolbar>
+      </AppBar>
+      <br/>
+      <TextField onChange ={(event) => setP(event.target.value)} id="outlined-basic" label="Enter Principal" variant="outlined" />
+      <TextField onChange ={(event) => sett(event.target.value)} id="outlined-basic" label="Enter Time" variant="outlined" />
+      <TextField onChange ={(event) => setr(event.target.value)} id="outlined-basic" label="Enter Rate" variant="outlined" />
+      <br/><br/>
+      <Button onClick = {()=> {calculateInterest()}} variant="contained">Calculate</Button>
+      <br/>
+      <Typography variant="h6" gutterBottom>
+         Simple Interest is {si}
+      </Typography>
     </div>
   );
 }
